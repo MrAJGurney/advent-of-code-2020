098f390f0fe0874695c6172585b9a4c9242a4d27
Andrew Gurney <AGurney@FindMyPast.com>
2020-12-16 21:46:18 +0000

    Add solution for day 16 part 2

--- puzzles/16/index.ts
+++ puzzles/16/index.ts
@@ -75,7 +75,161 @@ const isValid = (
 );
 
 const solveSecondPuzzle: PuzzleSolver = () => {
-	throw new Error('TODO');
+	const puzzleInput = readPuzzleInput(__dirname);
+	const [
+		rawRules,
+		rawYourTicket,
+		rawNearbyTickets,
+	] = puzzleInput.trim().split('\n\n');
+
+	const rules = guaranteeDefined(rawRules)
+		.trim()
+		.split('\n')
+		.reduce(
+			(
+				rules: {
+					[name: string]: Array<{min: number, max: number}>
+				},
+				rawRule
+			) => {
+				const [ruleName, ruleRanges, ] = rawRule.split(': ');
+				const rawRanges = guaranteeDefined(ruleRanges)
+					.trim()
+					.split(' or ');
+
+				rules[guaranteeDefined(ruleName)] = rawRanges.reduce(
+					(
+						ranges: Array<{min: number, max: number}>,
+						rawRange: string
+					) => {
+						const [min, max, ] = rawRange
+							.trim()
+							.split('-')
+							.map(value => parseInt(value));
+						ranges.push({
+							min: guaranteeDefined(min),
+							max: guaranteeDefined(max),
+						});
+						if (guaranteeDefined(max) < guaranteeDefined(min)) {
+							throw new Error('Maximum is smaller than minimum');
+						}
+						return ranges;
+					}, []);
+
+				return rules;
+			}, {}
+		);
+
+	const yourTicket = guaranteeDefined(rawYourTicket)
+		.trim()
+		.split('\n')
+		.slice(1)
+		.map(
+			ticket => ticket
+				.trim()
+				.split(',')
+				.map(value => parseInt(value))
+		)[0];
+
+	const nearbyTickets = guaranteeDefined(rawNearbyTickets)
+		.trim()
+		.split('\n')
+		.slice(1)
+		.map(
+			ticket => ticket
+				.trim()
+				.split(',')
+				.map(value => parseInt(value))
+		)
+		.filter(ticket => ticket
+			.every(
+				value => isValid(
+					value,
+					Object.values(rules).flat()
+				)
+			)
+		);
+
+	const rulesWithValidIndices = Object
+		.keys(rules)
+		.reduce(
+			(
+				rulesWithValidIndices: { [rule: string]: Array<number>, },
+				ruleKey
+			) => {
+				const validIndices = guaranteeDefined(yourTicket)
+					.reduce(
+						(
+							validIndices: Array<number>,
+							yourTicketValue,
+							index
+						) => {
+							const isValidRuleForYourTicket = isValid(
+								yourTicketValue,
+								guaranteeDefined(rules[ruleKey])
+							);
+							const isValidRuleForNearbyTickets = nearbyTickets
+								.every(
+									ticket =>
+										isValid(
+											guaranteeDefined(ticket[index]),
+											guaranteeDefined(rules[ruleKey])
+										)
+								);
+							if (
+								isValidRuleForYourTicket &&
+									isValidRuleForNearbyTickets
+							) {
+								validIndices.push(index);
+							}
+							return validIndices;
+						}, []
+					);
+
+				rulesWithValidIndices[ruleKey] = validIndices;
+				return rulesWithValidIndices;
+			}, {}
+		);
+
+	const assignedIndices: Array<number> = [];
+
+	const rulesWithIndex: {[ruleKey: string]: number} = Object
+		.keys(rulesWithValidIndices)
+		.sort((a, b) =>
+			guaranteeDefined(rulesWithValidIndices[a]).length -
+			guaranteeDefined(rulesWithValidIndices[b]).length
+		)
+		.reduce(
+			(
+				rulesWithIndex: {[ruleKey: string]: number},
+				ruleKey: string
+			) => {
+				const validIndex = guaranteeDefined(
+					guaranteeDefined(rulesWithValidIndices[ruleKey])
+						.find(index => !assignedIndices.includes(index))
+				);
+				rulesWithIndex[ruleKey] = validIndex;
+				assignedIndices.push(validIndex);
+				return rulesWithIndex;
+			}, {}
+		);
+
+	return Object
+		.keys(rulesWithIndex)
+		.filter(ruleKey => ruleKey.startsWith('departure '))
+		.reduce(
+			(
+				product: number,
+				ruleKey: string
+			) => {
+				const index = guaranteeDefined(rulesWithIndex[ruleKey]);
+				const value = guaranteeDefined(
+					guaranteeDefined(yourTicket)[index]
+				);
+
+				return product * value;
+			}, 1)
+		.toString();
 };
 
 const stars: Stars = {
